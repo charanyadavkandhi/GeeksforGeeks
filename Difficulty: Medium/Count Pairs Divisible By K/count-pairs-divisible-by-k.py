9d63f8f72552481d79ea8c7fa4aae8c1c4de994d
Kandhi Charan Yadav <charanyadavkandhi30@gmail.com>
2026-07-09 07:42:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Pairs Divisible By K/count-pairs-divisible-by-k.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countKdivPairs(self, arr, k):
+        freq = [0] * k
+        ans = 0
+
+        for x in arr:
+            r = x % k
+            ans += freq[(k - r) % k]
+            freq[r] += 1
+
+        return ans
