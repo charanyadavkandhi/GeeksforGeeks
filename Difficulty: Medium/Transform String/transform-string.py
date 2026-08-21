a1af42e9f4a7df42327248ecc8d58c98fe23673a
Kandhi Charan Yadav <charanyadavkandhi30@gmail.com>
2026-08-21 15:36:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Transform String/transform-string.py
+++ Difficulty: Medium/Transform String/transform-string.py
@@ -4,29 +4,21 @@ class Solution:
         if len(s1) != len(s2):
             return -1
 
-        # Check whether both strings contain the same characters
-        freq = [0] * 256
-
-        for ch in s1:
-            freq[ord(ch)] += 1
-
-        for ch in s2:
-            freq[ord(ch)] -= 1
-
-        for x in freq:
-            if x != 0:
-                return -1
+        # Check if both strings contain the same characters
+        if sorted(s1) != sorted(s2):
+            return -1
 
         i = len(s1) - 1
         j = len(s2) - 1
-        ans = 0
+        count = 0
 
-        while i >= 0 and j >= 0:
+        # Match characters from the end
+        while i >= 0:
             if s1[i] == s2[j]:
                 i -= 1
                 j -= 1
             else:
-                ans += 1
+                count += 1
                 i -= 1
 
-        return ans
+        return count
