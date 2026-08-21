21f4e052dc0cf02345819d8f744e440e7b952c65
Kandhi Charan Yadav <charanyadavkandhi30@gmail.com>
2026-08-21 15:31:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Transform String/transform-string.py
+++ Difficulty: Medium/Transform String/transform-string.py
@@ -4,19 +4,29 @@ class Solution:
         if len(s1) != len(s2):
             return -1
 
-        # Both strings must contain the same characters
-        if sorted(s1) != sorted(s2):
-            return -1
+        # Check whether both strings contain the same characters
+        freq = [0] * 256
+
+        for ch in s1:
+            freq[ord(ch)] += 1
+
+        for ch in s2:
+            freq[ord(ch)] -= 1
+
+        for x in freq:
+            if x != 0:
+                return -1
 
         i = len(s1) - 1
         j = len(s2) - 1
+        ans = 0
 
-        # Find the longest suffix of s1 that is already
-        # in the correct relative order in s2
         while i >= 0 and j >= 0:
             if s1[i] == s2[j]:
+                i -= 1
                 j -= 1
-            i -= 1
+            else:
+                ans += 1
+                i -= 1
 
-        # Characters not matched need to be moved to front
-        return j + 1
+        return ans
